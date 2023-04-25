7813a9f771df26dcdc612b5b4dd3358e23a2544a
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-04-25 11:12:39 +0000

    fix the return value of _printf

--- test/m.c
+++ test/m.c
@@ -1,13 +1,20 @@
 #include "main.h"
+#include <stdio.h>
 
 int main(void)
 {
 
+	int len, len1, len3, len4;
+
 	_printf("this is a trial\n");
-	_printf("this is a char %c\n", 'H');
+	len1 = _printf("this is a char %c\n", 'H');
+	len = printf("this is a char %c\n", 'H');
+	printf("len1 is %d, len is %d\n", len1, len);
 	_printf("this %c is a char\n", 'R');
 	_printf("%c is a char\n", 'R');
-	_printf("this is a string: %s\n", "try try try");
+	len3 = _printf("this is a string: %s\n", "try try try");
+	len4 = printf("this is a string: %s\n", "try try try");
+	printf("len1 is %d, len is %d\n", len3, len4);
 	_printf("%c this is a string: %s\n", 'Q', "pi, pi, pi");
 	_printf("this is %sa string: %s\n", "try try try", "pi, pi, pi");
 	_printf("this is a percent [%%]\n");

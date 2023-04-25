01329dd030b0fc4ecf99ffc4a2afe15d5f83603a
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-04-25 10:19:24 +0000

    printf project

---
+++ test/_printf.c
@@ -0,0 +1,59 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+
+/**
+ * _printf - prints to stdout format
+ * @format: string to print
+ * Return: Length of the string
+ */
+
+int _printf(const char *format, ...)
+{
+	int len = 0;
+	int i;
+	va_list ap;
+	char specifier;
+	char *str_to_print;
+
+	va_start(ap, format);
+
+	while (format[len] != '\0')
+	{
+		if (format[len] == '%')
+		{
+			specifier = format[len + 1];
+
+			if (specifier == 'c')
+			{
+				_putchar(va_arg(ap, int));
+				len = len + 2;
+			}
+
+			if (specifier == 's')
+			{
+				str_to_print = va_arg(ap, char *);
+				for (i = 0; str_to_print[i] != '\0'; i++)
+				{
+					_putchar(str_to_print[i]);
+				}
+				len += 2;
+			}
+			
+			if (specifier == '%')
+			{
+				_putchar('%');
+				len = len + 2;
+			}
+
+			if (specifier == 'd')
+			{
+
+		}
+		_putchar(format[len]);
+		len++;
+	}
+	va_end(ap);
+	return (len);
+}
---
+++ test/m.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+int main(void)
+{
+
+	_printf("this is a trial\n");
+	_printf("this is a char %c\n", 'H');
+	_printf("this %c is a char\n", 'R');
+	_printf("%c is a char\n", 'R');
+	_printf("this is a string: %s\n", "try try try");
+	_printf("%c this is a string: %s\n", 'Q', "pi, pi, pi");
+	_printf("this is %sa string: %s\n", "try try try", "pi, pi, pi");
+	_printf("this is a percent [%%]\n");
+	_printf("percent is [%%] a trial\n");
+	return (1);
+}

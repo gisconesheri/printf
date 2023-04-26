d4969c02dd89a26c846f85200954a19055c6bc74
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-04-26 17:16:40 +0000

    fix the binary special character

--- main.h
+++ main.h
@@ -5,5 +5,5 @@ int _printf(const char *format, ...);
 int _putchar(char c);
 int print_number(int *pos, long int number);
 int num_to_char(int num);
-int print_binary(int *pos, unsigned int number);
+int print_binary(int *pos, int number);
 #endif

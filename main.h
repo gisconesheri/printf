6d633db04297c55933cbe7d97e054b6b27d2edbb
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-05-06 19:46:25 +0000

    modify functionality for print_string

--- main.h
+++ main.h
@@ -3,7 +3,9 @@
 
 int _printf(const char *format, ...);
 int _putchar(char c);
+int print_str(int *pos, char *str_to_print);
 int print_number(int *pos, long int number);
 int num_to_char(int num);
 int print_binary(int *pos, int number);
+int print_char(int *pos, int char_to_print);
 #endif
